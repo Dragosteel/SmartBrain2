168798ab155161780299c88b317cb9988ab0cec3
Dragosteel <bebo4money@gmail.com>
2023-12-26 16:39:49 +0200

    Added Another Particles

--- src/Components/ParticlesJS/ParticlesJS.js
+++ src/Components/ParticlesJS/ParticlesJS.js
@@ -4,7 +4,6 @@ import Particles, { initParticlesEngine } from "@tsparticles/react";
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-import options from "./Snow.ts";
 
 const ParticlesJS = () => {
     const [init, setInit] = useState(false);
@@ -24,13 +23,82 @@ const ParticlesJS = () => {
         });
     }, []);
 
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
     return (
-        { init },
-        < Particles
+        { init }, <Particles
             id="tsparticles"
-            options={options}
+            particlesLoaded={particlesLoaded}
+            options={{
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
         />
-    );
-}
+    )
+        ;
+};
+
 
 export default ParticlesJS;
